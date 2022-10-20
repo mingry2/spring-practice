773400b0fe0ec03afb7ddfc27ca41f307e642943
minkyoung kim <minry20415@naver.com>
2022-10-20 15:42:19 +0900

    [update] spring --deleteAll(), getCount() 예외처리

--- src/main/java/com/likelion/dao/UserDao.java
+++ src/main/java/com/likelion/dao/UserDao.java
@@ -45,16 +45,37 @@ public class UserDao {
     }
 
     public int getCount() throws SQLException {
-        Connection conn = connectionMaker.makeConnection();
-        PreparedStatement ps = conn.prepareStatement("select count(*) from users");
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
-        rs.close();
-        ps.close();
-        conn.close();
-        return count;
+        try {
+            conn = connectionMaker.makeConnection();
+            ps = conn.prepareStatement("select count(*) from users");
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (rs != null)
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+                if (conn != null) {
+                    try {
+                        conn.close();
+                    } catch (SQLException e) {
+                    }
+                }
+            }
+        }
     }
 
     public void add(User user) {
